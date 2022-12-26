6256e89f78b9c16a3937acdb8af0b0348ddbdff2
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-12-26 23:02:05 +0100

    Added rollup

---
+++ dist/types/stories/Button.stories.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="react" />
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+declare const _default: ComponentMeta<({ color }: {
+    color: "primary" | "secondary";
+}) => JSX.Element>;
+export default _default;
+export declare const Primary: ComponentStory<({ color }: {
+    color: "primary" | "secondary";
+}) => JSX.Element>;
+export declare const Secondary: ComponentStory<({ color }: {
+    color: "primary" | "secondary";
+}) => JSX.Element>;
---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import { uglify } from 'rollup-plugin-uglify';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+
+import packageJson from './package.json' assert { type: 'json' };
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript(),
+    postcss({
+      extensions: ['.css', '.scss'],
+    }),
+    uglify(),
+    babel(),
+    commonjs(),
+    image(),
+  ],
+};
