8b6123131c6e1941f20c287dfdef344c5a4595d6
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-12-26 22:32:24 +0100

    Added dep and first component

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = () => {
+  return <button className="button">Button</button>;
+};
+
+export default Button;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Atoms
+export { default as Button } from './atoms/Button/Button';
+
+// Molecules
+
+// Organisms
