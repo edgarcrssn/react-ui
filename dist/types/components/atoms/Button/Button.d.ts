42631a25e742cd37a95ca77aebece3f9cab544b0
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2022-12-27 15:16:18 +0100

    Add dist to gitignore

--- dist/types/components/atoms/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import './Button.scss';
-type Props = {
-    color: 'primary' | 'secondary';
-};
-declare const Button: ({ color }: Props) => JSX.Element;
-export default Button;
